d1998375cb6124e87e0f87a8360f734f01cf3e79
SannaLundqvist <lundqvist174@gmail.com>
2019-11-28 11:39:25 +0100

    exte back

--- src/main/java/com/example/demo/ExteBackApplication.java
+++ src/main/java/com/example/demo/ExteBackApplication.java
@@ -3,10 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import se.hig.exte.model.Exam;
+
 @SpringBootApplication
 public class ExteBackApplication {
 
 	public static void main(String[] args) {
+		Exam exam = new Exam();
+		System.out.println(exam.equals(new Exam()));
 		SpringApplication.run(ExteBackApplication.class, args);
 	}
 
